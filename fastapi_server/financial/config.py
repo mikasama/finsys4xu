ebf0bf4abcb3173be1ec026031a2bce9ae238206
mikasama <459656481@qq.com>
2022-07-31 00:19:23 +0800

    增加数据库连接文件、配置文件

---
+++ fastapi_server/financial/config.py
@@ -0,0 +1,5 @@
+db_name = 'test'
+db_user = 'root'
+db_passwd = 'Xu123456'
+db_port = 3306
+db_url = '172.17.0.4'
---
+++ fastapi_server/financial/database.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = '__mikasama__'
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import config
+
+
+db_url: str = f'mysql+pymysql://{config.db_user}:{config.db_passwd}@{config.db_url}:{config.db_port}/{config.db_name}'  # MySql\PostgreSQL连接方法
+
+engine = create_engine(
+    db_url, encoding='utf-8', echo=True  # echo=True表示引擎将用repr()函数记录所有语句及其参数列表到日志
+)
+
+# sqlalchemy中，CRUD都是通过会话(session)进行的，所以我们必须要先创建会话，每一个SessionLocal实例就是一个数据库session
+# flush()是指发送数据库语句到数据库，但数据库不一定执行写入磁盘；commit()是指提交事务，将变更保存到数据库文件
+# 当 expire_on_commit=True 时，commit 之后所有实例都会过期，之后再访问这些过期实例的属性时，SQLAlchemy 会重新去数据库加载实例对应的数据记录。高并发出现性能下降，可尝试设置为false
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=True)
+
+# 创建基本映射类，应该叫实例。用于创建数据库表
+Base = declarative_base(bind=engine, name='Base')
